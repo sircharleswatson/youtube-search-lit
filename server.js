a216641bc258fac879bb7d2b52e6b59e461e764f
sircharleswatson <sircharleswatson@gmail.com>
2023-12-07 00:13:36 -0600

    setup search endpoint and youtube module
    Also adds a cache using @fastify/leveldb

--- server.js
+++ server.js
@@ -1,18 +1,63 @@
-const fastify = require("fastify")({ logger: true });
-const path = require("path");
+const fastify = require("fastify")({ logger: true })
+const axios = require("axios")
+const path = require("path")
+const fastifyEnv = require("@fastify/env")
+const fastifyLeveldb = require("@fastify/leveldb")
+const fastifyStatic = require("@fastify/static")
 
-fastify.register(require("@fastify/static"), {
+const youtube = require("./youtube")
+
+const schema = {
+  type: "object",
+  required: ["YOUTUBE_API_KEY"],
+  properties: {
+    YOUTUBE_API_KEY: {
+      type: "string",
+    },
+  },
+}
+
+const options = {
+  schema,
+  dotenv: true,
+}
+
+fastify
+  .register(fastifyEnv, options)
+  .ready()
+
+fastify.register(
+  fastifyLeveldb,
+  { name: "db" },
+)
+
+fastify.post("/api/search", async function cb(request, reply) {
+  const { term, order } = request.body
+
+  const videos = await youtube.search(term, order, {context: this})
+
+  reply.send(videos)
+})
+
+fastify.get("/api", (request, reply) => {
+  reply.send({ ping: "pong" })
+})
+
+fastify.register(fastifyStatic, {
   root: path.join(__dirname, "dist"),
   prefix: "/",
-});
+})
 
-fastify.setNotFoundHandler(function (request, reply) {
-  reply.sendFile("index.html");
-});
+fastify.setNotFoundHandler((request, reply) => {
+  reply.sendFile("index.html")
+})
 
 fastify.listen(
-  { port: process.env.PORT || 3000, host: process.env.HOST || "0.0.0.0" },
-  (err, address) => {
-    if (err) throw err;
-  }
-);
+  {
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || "0.0.0.0",
+  },
+  (err) => {
+    if (err) throw err
+  },
+)
---
+++ youtube.js
@@ -0,0 +1,130 @@
+const axios = require("axios")
+const crypto = require("crypto")
+
+/*
+ * Public API
+ */
+
+async function search(term, order = "relevance", { context }) {
+  console.log("TERM", term)
+  console.log("ORDER", order)
+  const cachedResults = await searchCache(term, order, { context })
+
+  if (cachedResults) {
+    console.log("USING CACHE")
+    return cachedResults
+  }
+
+  return searchYoutube(term, order, { context })
+}
+
+/*
+ * Private
+ */
+
+async function searchCache(term, order, { context }) {
+  const searchKey = hashSearchArgs(term, order)
+  console.log(searchKey)
+  try {
+    return JSON.parse(await context.level.db.get(searchKey))
+  } catch (e) {
+    return null
+  }
+}
+
+async function searchYoutube(term, order, { context }) {
+  console.log("SEARCHING YOUTUBE")
+  const queryString = getQueryString({
+    key: process.env.YOUTUBE_API_KEY,
+    q: term,
+    order,
+    maxResults: 50,
+    part: "snippet",
+  })
+
+  const searchResults = await searchVideos(queryString)
+  const enrichedVideos = await enrichVideos(searchResults)
+
+  await cacheVideos(term, order, enrichedVideos, {context})
+
+  return enrichedVideos
+}
+
+async function enrichVideos(videos) {
+  const ids = extractVideoIds(videos).join(",")
+  return getVideoDetails(ids)
+}
+
+async function cacheVideos(term, order, videos, { context }) {
+  const searchKey = hashSearchArgs(term, order)
+  console.log(searchKey)
+
+  await context.level.db.put(searchKey, JSON.stringify(videos))
+}
+
+/*
+ * Youtube API
+ */
+
+async function searchVideos(queryString) {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search${queryString}`)
+    return response.data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+async function getVideoDetails(ids) {
+  const queryParams = {
+    key: process.env.YOUTUBE_API_KEY,
+    id: ids,
+    part: "snippet,contentDetails,statistics",
+  }
+
+  const queryString = `?${new URLSearchParams(queryParams).toString()}`
+
+  try {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos${queryString}`)
+    const cleanedVideos = getVideoFields(
+      response.data.items, 
+      ["id", "snippet", "statistics", "contentDetails"]
+    )
+
+    return cleanedVideos
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+/*
+ * Helpers
+ */
+
+function getQueryString(params) {
+  const queryString = `?${new URLSearchParams(params).toString()}`
+  console.log("QUERY STRING: ", queryString)
+  return queryString
+}
+
+function hashSearchArgs(term, order) {
+  const searchArgString = [term, order].join(",")
+
+  return crypto.createHash("sha1")
+    .update(searchArgString).digest("hex")
+}
+
+function getVideoFields(videos, fields) {
+  return videos.map((video) => fields.reduce((result, key) => ({
+    ...result,
+    [key]: video[key],
+  }), {}))
+}
+
+function extractVideoIds(searchResults) {
+  return searchResults.items.map((video) => video.id.videoId)
+}
+
+module.exports = {
+  search,
+}
